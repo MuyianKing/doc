ff1c95d1ce7510c09adfdc1ce9bc1f6e0dee964f
MuyianKing <muyianking@163.com>
2024-12-13 14:26:57 +0800

    update

---
+++ assets/docs_rules_uni.md.Cd-ZetwR.js
@@ -0,0 +1,19 @@
+import{aw as n,t as a,aV as p,q as e}from"./chunks/framework.DqQnpCOn.js";const m=JSON.parse('{"title":"uni-app目录结构","description":"","frontmatter":{"title":"uni-app目录结构"},"headers":[],"relativePath":"docs/rules/uni.md","filePath":"docs/rules/uni.md","lastUpdated":1723624027000}'),l={name:"docs/rules/uni.md"};function i(t,s,c,o,d,r){return e(),a("div",null,s[0]||(s[0]=[p(`<div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>uni-app</span></span>
+<span class="line"><span>|── public //公共资源目录</span></span>
+<span class="line"><span>|── src</span></span>
+<span class="line"><span>|   |── api </span></span>
+<span class="line"><span>|   |   |── index.js 统一管理接口</span></span>
+<span class="line"><span>|   |── components 公共组件</span></span>
+<span class="line"><span>|   |── default 公共待选项</span></span>
+<span class="line"><span>|   |── model 实体类</span></span>
+<span class="line"><span>|   |── pages 页面</span></span>
+<span class="line"><span>|   |── server</span></span>
+<span class="line"><span>|   |── static 资源目录</span></span>
+<span class="line"><span>|   |── store Vuex</span></span>
+<span class="line"><span>|   |── uni_modules 第三方组件</span></span>
+<span class="line"><span>|   |── util 公共js代码</span></span>
+<span class="line"><span>|── App.vue</span></span>
+<span class="line"><span>|── main.js</span></span>
+<span class="line"><span>|── env.js 环境变量</span></span>
+<span class="line"><span>|── uni.scss css变量</span></span>
+<span class="line"><span>|── alias.config.js 别名配置</span></span></code></pre></div>`,1)]))}const f=n(l,[["render",i]]);export{m as __pageData,f as default};
